f7e03d14c0a57c4b52e995eda0ba0b36fc19b971
Juan_Taylor <jtaylor39@hawk.illinoistech.edu>
2025-11-06 14:53:30 -0600

    index

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ function init(){
 //add your javascrip between these two lines of code
  
 
-
-
-
-
+let entrybutton = 
+function entrybutton() {
+  alert("I am an alert!");
+}
 
 
 window.addEventListener('load', init);
