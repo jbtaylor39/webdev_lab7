59b8311d3445f7729a420efc531449a8be605cb5
Juan_Taylor <jtaylor39@hawk.illinoistech.edu>
2025-11-11 14:55:12 -0600

    index

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 function init(){
 var alertbox = document.getElementById("entrybutton").addEventListener("click", popup);
-var textentry = document.getElementById("entryinput").value;
 }
 
 function popup() {
-   alert("Juan: " + textentry);
+    var textentry = document.getElementById("entryinput").value;
+    let textoutput = document.getElementById("entryinput").value;
+    alert("Juan: " + textentry);
     document.getElementById
 }
 
